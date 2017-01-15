bb00f29789decf4539770f3ffd571007359d7321
Stan Sajous <stan.sajous@gmail.com>
2017-01-15 11:25:22 -0700

    even prettier

--- src/Todo.Web/ClientApp/app/app.module.ts
+++ src/Todo.Web/ClientApp/app/app.module.ts
@@ -3,10 +3,6 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component'
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 import { TodoComponent } from './components/todo/todo.component';
 
 import { TodoService } from './components/todo/todo.service';
@@ -15,22 +11,14 @@ import { TodoService } from './components/todo/todo.service';
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent,
         TodoComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         FormsModule,
         RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: 'todo', component: TodoComponent },
-            { path: '**', redirectTo: 'home' }
+            { path: '', component: TodoComponent },
+            { path: '**', redirectTo: '' }
         ])
     ],
     providers: [ TodoService ]
